5f7817b915673c4fa6628385acf753eb7c1ba597
aamcodes <aaronmendezcodes@gmail.com>
2023-03-09 00:53:03 -0500

    Added CRUD functionality for magicians allowing for the creation, deletion and updating for musician routes across the application.

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const { sequelize } = require('./db');
 
 const port = 3000;
 
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
 //TODO
 app.get('/musicians', async (req, res) => {
 	try {
@@ -28,6 +31,67 @@ app.get('/musicians/:id', async (req, res) => {
 			})
 			.catch((err) => {
 				res.status(404).json({ message: 'Musician cannot be found' });
+			});
+	} catch (err) {
+		res.status(500).json({ message: 'Server Internal Error' });
+	}
+});
+
+app.post('/musicians', async (req, res) => {
+	try {
+		let { name, instrument } = req.body;
+		if (!name || !instrument) {
+			res.status(400).json({ message: 'All fields are required' });
+		} else {
+			await Musician.create({ name, instrument })
+				.then((musician) => {
+					res.status(201).json(musician);
+				})
+				.catch((err) => {
+					res.status(500).json({
+						message: 'Database Internal Error',
+					});
+				});
+		}
+	} catch (err) {
+		res.status(500).json({ message: 'Server Internal Error' });
+	}
+});
+
+app.put('/musicians/:id', async (req, res) => {
+	try {
+		let { id } = req.params;
+		let { name, instrument } = req.body;
+		if (!name || !instrument) {
+			res.status(400).json({ message: 'All fields are required' });
+		}
+		await Musician.findByPk(id)
+			.then((musician) => {
+				musician.set({
+					name,
+					instrument,
+				});
+				musician.save();
+				res.status(201).json(musician);
+			})
+			.catch((err) => {
+				res.status(404).json({ message: 'Musician not found' });
+			});
+	} catch (err) {
+		res.status(500).json({ message: 'Server Internal Error' });
+	}
+});
+
+app.delete('/musicians/:id', async (req, res) => {
+	try {
+		let { id } = req.params;
+		await Musician.findByPk(id)
+			.then((musician) => {
+				musician.destroy();
+				res.status(201).json({ removed: musician });
+			})
+			.catch((err) => {
+				res.status(404).json({ message: 'No musician found' });
 			});
 	} catch (err) {
 		res.status(500).json({ message: 'Server Internal Error' });
